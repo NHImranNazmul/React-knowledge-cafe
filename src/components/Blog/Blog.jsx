a0e8e2877de7f59d772c9ce3d6a50ca1d49a122a
NH Imran <nazmulnhimran@gmail.com>
2024-10-06 17:02:25 +0600

    Add booked marked blog and reading time

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,16 +1,46 @@
 import PropTypes from 'prop-types'
+import { FaBookmark } from 'react-icons/fa';
 
-const Blog = ({blog}) => {
-    const {title}= blog;
+const Blog = ({blog, handleAddToBookmark, handleMarkAsRead}) => {
+    const {title, cover, author, author_img, posted_date, reading_time, hashtag}= blog;
     return (
-        <div>
+        <div className='mb-20 ml-20 space-y-4'>
+            <img className='w-full mb-8' src={cover} alt={`cover picture of title ${title}`} />
+            <div className='flex justify-between mb-4'>
+                <div className='flex'>
+                    <img className='w-12 rounded-full' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className="text-2xl">{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                    <button 
+                    onClick={() =>handleAddToBookmark(blog)}
+                    className='ml-2 text-red-400'
+                    ><FaBookmark></FaBookmark></button>
+                </div>
+            </div>
             <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtag.map((hash, idx) => <span key={idx}><a href=''>#{hash}</a> </span>)
+                }
+            </p>
+            <button 
+            onClick={()=> handleMarkAsRead(reading_time)}
+            className='text-green-600 font-bold underline'
+            >Mark as Read</button>
         </div>
     );
 };
 
 Blog.propTypes ={
-    blog: PropTypes.object.isRequired
+    blog: PropTypes.object.isRequired,
+    handleAddToBookmark: PropTypes.func,
+    handleMarkAsRead: PropTypes.func
+    
 }
 
 export default Blog;
